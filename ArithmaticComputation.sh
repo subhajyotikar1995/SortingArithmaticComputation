f8fad890bfca3d164b0ad8b6caad88a733358566
subhajyoti <subhajyotikar0@gmail.com>
2020-10-28 13:15:44 +0530

    check for expression1

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -3,3 +3,10 @@
 read -p "Enter Value of A : " a
 read -p "Enter Value of B : " b
 read -p "Enter Value of C : " c
+
+if [[ $a ]] && [[ $b ]] && [[ $c ]] && [ $a -eq $a 2>/dev/null ] && [ $b -eq $b 2>/dev/null ] && [ $c -eq $c 2>/dev/null ]; then
+	result=$(( a + b * c ))
+	echo $result
+else
+     echo "Wrong Input!"
+fi
